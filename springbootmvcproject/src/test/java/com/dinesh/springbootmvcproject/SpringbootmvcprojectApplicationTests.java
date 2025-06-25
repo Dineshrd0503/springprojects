d111f5da348bd7cecf31aa5562cea321c816e1d7
dinesh rd <ananddinesh00399@gmail.com>
2025-06-25 14:18:37 +0530

    "new files added"

---
+++ springbootmvcproject/src/test/java/com/dinesh/springbootmvcproject/SpringbootmvcprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.springbootmvcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmvcprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
