6a522e547fb7913d9942f2c92900d6c09ec46b0d
Dineshrd0503 <ananddinesh00399@gmail.com>
2025-10-19 21:09:26 +0530

    Delete telecominventory directory

--- telecominventory/src/main/java/com/dinesh/telecominventory/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dinesh.telecominventory.repository;
-
-import com.dinesh.telecominventory.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
